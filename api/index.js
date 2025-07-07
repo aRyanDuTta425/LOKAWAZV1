f02c53f4b906a3196325c7ff8d4e14ba301031aa
Aryan Dutta <aryandutta425@gmail.com>
2025-07-08 01:10:00 +0530

    Update Vercel configuration for deployment

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
 // Vercel serverless function handler
+const path = require('path');
+
+// Set the correct path for the server files
+process.env.NODE_PATH = path.join(__dirname, '..', 'server');
+
 const app = require('../server/app');
 
 module.exports = app;
